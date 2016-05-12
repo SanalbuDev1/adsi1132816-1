340233dd416073b3852b6db859f8166f4d92c6fb
llolo121221 <salvarezb@uniremingtonmanizales.edu.co>
2016-05-12 12:44:56 -0500

    html5

--- html/html/codigo.js
+++
@@ -1,9 +0,0 @@
-
-var nombre = prompt('Cual es su nombre?');
-
-if(nombre == 'santiago') {
-	alert('Bienvenido: '+nombre+' el papasito violame <3');
-} else {
-	alert('Bienvenido: '+nombre+' la pedorra');
-
-}
